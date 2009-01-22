554c46854f965822bc4241742ad46f57824e9b68
Moritz Ulrich <thekenny123@gmail.com>
2009-01-22 14:05:42 +0100

    Changed #17 to a unified style too.

--- pe17.lisp
+++ pe17.lisp
@@ -8,11 +8,10 @@
 
 (defun length-of-number (n)
   (let ((string (format nil "~r" n)))
-    (print string)
     (+ (if (and (> n 100) (not (zerop (mod n 100)))) 3 0) ; If the last two digits are smaller than 100, we have to add 3 for the "and" like "one hundred *and* then
 	(count-if #'(lambda (x) (not (or (char-equal x #\-) (char-equal x #\Space)))) string))))
 
-(defun length-of-numbers-to (n)
-  (loop for i from 1 to n
+(defun euler17 ()
+  (loop for i from 1 to 1000
         for length = (length-of-number i)
         summing length))

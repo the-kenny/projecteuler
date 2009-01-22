a70cb2ed83c7fcacb39b41ac746efe65b06868cb
Moritz Ulrich <thekenny123@gmail.com>
2009-01-22 14:03:51 +0100

    Changed the form of #1 to a unified style (euler<num>) w.o. arguments

--- pe1.lisp
+++ pe1.lisp
@@ -5,9 +5,9 @@
 ;;; By Moritz Ulrich <ulrich (dot) moritz (at) googlemail.com>
 ;;;
 
-(defun sum-naturals (n &optional (acc 0))
-  (cond ((= n 0) acc)
-        ((or (= 0 (mod n 3)) (= (mod n 5) 0)) (sum-naturals (1- n) (+ acc n)))
-        (t (sum-naturals (1- n) acc))))
-
-(print (sum-naturals 999))
+(defun euler1 ()
+  (labels ((sum-naturals (n &optional (acc 0))
+						 (cond ((= n 0) acc)
+							   ((or (= 0 (mod n 3)) (= (mod n 5) 0)) (sum-naturals (1- n) (+ acc n)))
+							   (t (sum-naturals (1- n) acc)))))
+	(sum-naturals 999)))
